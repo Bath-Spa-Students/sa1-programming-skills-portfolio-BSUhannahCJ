6d9c2b87d16a7fba3871b53dee10666d04eb9b3c
BSUhannahCJ <hannahchanjudeh@gmail.com>
2024-10-17 10:05:17 +0400

    Simple Sums
    Assessment 1 Exercise 2

---
+++ Exercise-02/Simple Sums.py
@@ -0,0 +1,6 @@
+#Exercise 2: simple sums
+
+Hi = 8 #declaring the variable 'Hi' with an integer value of 8 as the first operant
+Hey = 10 #declaring the variable 'Hey' with an integer value of 10 as the second operant
+Hello = Hi + Hey #Variable 'Hello' is declared as the sum of 'Hi' and 'Hey' by using the + operator
+print (Hello) #so when we print C, it will print the sum of both variables
