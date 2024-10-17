d4a4017b95b5450c22f381099790ac6ff18468b2
BSUhannahCJ <hannahchanjudeh@gmail.com>
2024-10-17 10:03:17 +0400

    Coding is Cool
    Assessment 1 Exercise 1

---
+++ Exercise-01/Coding is Cool.py
@@ -0,0 +1,9 @@
+"""First exercise: string concatenation with the phrase 'Coding is Cool' """
+
+Word1 = "Coding " # first variable with the first word/string of the required phrase
+Word2 = "is " # second variable with the second word/string
+Word3 = "Cool" # third variable with the third word/string
+
+"""string concatenation through different variables by using the 
+'+' operator to add the variables together in one print command"""
+print(Word1 + Word2 + Word3)
