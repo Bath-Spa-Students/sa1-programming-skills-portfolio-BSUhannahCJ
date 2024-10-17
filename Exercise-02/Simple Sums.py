3dc8d32b6212fee00bcb671dfa3b26d4019f7824
BSUhannahCJ <hannahchanjudeh@gmail.com>
2024-10-17 10:15:45 +0400

    Simple Sums
    Assessment 1 exercise 02

--- Exercise-02/Simple Sums.py
+++ Exercise-02/Simple Sums.py
@@ -1,5 +1,12 @@
 #Exercise 2: simple sums
 
+"""Steps:
+1. Declare a variable and initialize it with the integer value `8`.
+2. Declare a second variable and initialize it with the integer value `10`.
+3. Declare a third variable that stores the sum of first two numbers.
+4. Print the value of the sum to the console.
+"""
+
 Hi = 8 #declaring the variable 'Hi' with an integer value of 8 as the first operant
 Hey = 10 #declaring the variable 'Hey' with an integer value of 10 as the second operant
 Hello = Hi + Hey #Variable 'Hello' is declared as the sum of 'Hi' and 'Hey' by using the + operator
