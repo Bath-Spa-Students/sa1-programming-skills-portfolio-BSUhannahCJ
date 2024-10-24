3c9f692ab769da7528a8bc51d77be6a9513261b7
BSUhannahCJ <hannahchanjudeh@gmail.com>
2024-10-24 10:09:12 +0400

    Standard Primitive Quiz
    assessment 1 exercise 4

---
+++ Exercise-04/Standard Primitive Quiz.py
@@ -0,0 +1,6 @@
+Qnum_1= input("What is the capital of France?: ") #first variable stores a question that is asking an answer/input from the user with the input command.
+print (Qnum_1) #print function will display the question on the screen, allowing the user to type in their question
+if Qnum_1 == "Paris": #if the user puts the correct answer which is 'Paris',
+    print("Correct answer!") # then 'correct answer' will display on screen
+else:
+    print("Wrong answer!") # else it will display 'wrong answer'
