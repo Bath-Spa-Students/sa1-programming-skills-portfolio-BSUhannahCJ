3e374bbc4c39139c478a212e5506353981f42f22
BSUhannahCJ <hannahchanjudeh@gmail.com>
2024-11-10 21:43:19 +0400

    Standard Biography
    assessment 3 exercise 3 standard requirements

---
+++ Exercise-03/Standard Biography.py
@@ -0,0 +1,18 @@
+''' Exercise 3: Biography - 25 Marks
+
+In this exercise, you'll create a program that stores and prints your name, hometown, and age to the console using a Python dictionary.
+
+ Steps:
+1. Store the information (name, hometown, and age) as key-value pairs in a dictionary.
+2. Print the values on separate lines using a single `print()` statement.
+3. Use variables with appropriate data types for each piece of information.
+
+'''
+#exercise 3 - Biography  
+
+My_Biography2024 = {'Name' : 'Hannah Judeh', 
+                    'Home-town' : 'Dubai',
+                    'Age' : '18 years old'} #Dictionary with my biography 
+
+#print concatenation of all items in the dictionary. They will display to the console.
+print("Name:", My_Biography2024["Name"], "\nHome-town:", My_Biography2024["Home-town"], "\nAge:", My_Biography2024["Age"])
