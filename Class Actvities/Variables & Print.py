2b21cf86721a1b9122dec8667679209cf481ec6f
BSUhannahCJ <hannahchanjudeh@gmail.com>
2024-11-11 12:21:04 +0400

    Variables & Print
    class activity

---
+++ Class Actvities/Variables & Print.py
@@ -0,0 +1,33 @@
+#variables practice. words are strings, numbers are integers and decimels are floats
+a=18
+b="hannah"
+c=18
+d="anagha"
+e=19
+f="denise"
+g=19
+h="elizabet"
+i=18
+j="zhea"
+k=19
+l="dawn"
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
+print(k)
+print(l)
+
+'''when the same variable has two different values,
+ it will print the latest one because the latest one
+will overwrite the previous one'''
+
+Hannah = 'banana'
+Hannah = 'coconut'
+print(Hannah)
