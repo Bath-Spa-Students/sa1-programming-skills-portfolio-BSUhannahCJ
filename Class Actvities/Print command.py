53dbad62526f3c6fb89aec160178f8db89125339
BSUhannahCJ <hannahchanjudeh@gmail.com>
2024-11-11 12:13:48 +0400

    print command
    class activity

---
+++ Class Actvities/Print command.py
@@ -0,0 +1,22 @@
+#this is my profile (first practice with print command)
+print("My name is hannah")
+print("I am 18 years old")
+print("I study creative computer")
+print("My institution is BathSpa University RAK")
+print("My mom's name is Bonnette and my dad's name is Rodulf")
+print("I live in Ajman")
+print("My hobbies are designing and baking")
+print("i have two cats and their names are Hunter and Sweety")
+print("i love cats and birds")
+print("i love my university")
+
+#this is our second class practice
+"""To have comments with 
+          more than 1 line, we use triple quotation marks."""
+
+print("""twinkle twinkle little star
+               how i wonder what you are""")
+print("""meow meow
+               bark bark""")
+print("""i love food
+               and i love sweets""")
