08e070f8b6d59be9526f68683bb3cc7fcc412a34
BSUhannahCJ <hannahchanjudeh@gmail.com>
2024-11-12 17:52:58 +0400

    if-elif-else
    class activity

---
+++ Class Actvities/if-elif-else.py
@@ -0,0 +1,10 @@
+#if-elif-else
+# its more professional, modern and saves coding lines.
+ai= 20 
+bee= 25
+if bee > ai:
+    print("bee is greater than ai")
+elif ai == bee:
+    print("ai and bee are equal")
+else:
+    print('ai is greater than bee')
