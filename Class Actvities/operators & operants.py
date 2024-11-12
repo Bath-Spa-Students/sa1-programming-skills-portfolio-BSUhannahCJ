4d0b01706993979fd9c8427206274a24136292cc
BSUhannahCJ <hannahchanjudeh@gmail.com>
2024-11-12 17:27:56 +0400

    operators & operants
    class activity

---
+++ Class Actvities/operators & operants.py
@@ -0,0 +1,15 @@
+#performing calculations
+#numbers are termed as operants 
+print (4 + 5)
+print (10 - 6)
+print (20 * 3)
+print (40/2) # for floating point
+print (60//2) # for integer
+
+# operator prescedence BODMAS (bracket, operator, division, multiplication, addition, and subtraction)
+print ((2*3)/5 + (23-5))
+
+#when u want to assign a long equation to a varaible or to print it but u dont want it in one line u use + \
+numz = 6 * 2 + 4 * + \
+    3 * 4 + 2 * 5
+print(numz)
