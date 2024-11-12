a3da5acb7765c5202c0697c59b8b2a4ffacc55c7
BSUhannahCJ <hannahchanjudeh@gmail.com>
2024-11-12 17:45:16 +0400

    if, if else
    class activity

---
+++ Class Actvities/if, if else.py
@@ -0,0 +1,16 @@
+#if command
+h =  33
+b = 200
+if b > h:
+    print("b is greater than h!")
+
+# == is equal to, = is assignment operator, != is not equal to, > greater than,< lesser than
+#if command has one condition and one decision/statement
+#if else command has one condition and two decisions/statements
+
+c= 40
+j= 10
+if c > j:
+    print("its cold!")
+else:
+    print("perfect weather!")
