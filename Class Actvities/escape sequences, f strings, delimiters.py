a992d19ea98218eb04a1c710510a194834e1834c
BSUhannahCJ <hannahchanjudeh@gmail.com>
2024-11-12 17:41:42 +0400

    escape sequences, f strings, delimiters
    class activity

---
+++ Class Actvities/escape sequences, f strings, delimiters.py
@@ -0,0 +1,12 @@
+# end delimited to remove new line, by default print will to another statement
+uh = "Hannah Judeh"
+print (uh, end ="")
+print(" new print line", end ="")
+
+# \n and \t these are also known as escape sequences
+print ("\n this line will print on next line")
+print ("\t this line will be tabbed")
+
+#f strings
+student = "hannah"
+print(f"Your name is {student}")
