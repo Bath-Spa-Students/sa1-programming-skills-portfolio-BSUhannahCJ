17677b1e56b105d9838808534e9ff1b3a81a6c54
BSUhannahCJ <hannahchanjudeh@gmail.com>
2024-11-19 22:08:37 +0400

    Is it even?
    assessment 1 exercise 10

---
+++ Exercise-10/Is it even.py
@@ -0,0 +1,23 @@
+#exercise 10
+'''Write a program that tests if a value is even or odd. Follow the instructions outlined below:
+
+### Instructions:
+* The program should ask the user for a number from within the main function.
+* The entered number should be passed to a function that determines if the value is even or odd.
+* The function should return a message indicating whether the number is even or odd.
+* The message returned by the function should be printed from within the main function.'''
+
+#passing an argument through this function
+def even_or_odd(NUM):
+    if NUM % 2 == 0:  #simple code to check if number is even or odd
+        print("It's an even number!")
+    else:
+        print("It's an odd number!")
+
+#the main function where the program starts
+def MAIN():
+    number = int(input("Enter a number: ")) #asking input
+    even_or_odd(number) #calling previous function 
+
+#calling the function
+MAIN()
