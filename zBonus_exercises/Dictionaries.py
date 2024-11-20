0906a5cd58c54f03cb086582f4ecda5a10c399e9
BSUhannahCJ <hannahchanjudeh@gmail.com>
2024-11-20 22:47:31 +0400

    Dictionaries
    Bonus exercise 6

---
+++ zBonus_exercises/Dictionaries.py
@@ -0,0 +1,14 @@
+'''Use a dictionary to store information about a person you know.Store their first name,
+last name, age, and the city in which they live.
+
+You should have keys such as first_name, last_name, age, and city. Print each piece of
+information stored in your dictionary.'''
+
+#Dictionary
+Mum = {'first_name' : 'Bonnette',
+       'last_name' : 'Chan',
+       'age' : '52',
+       'city' : 'Ajman'}
+
+#printing the items in the dictionary
+print(Mum.items())
