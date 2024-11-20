9ac82cfcded39df43d48c45cfe4fef27c670f7c6
BSUhannahCJ <hannahchanjudeh@gmail.com>
2024-11-20 22:23:16 +0400

    Variables
    Bonus exercise 2

---
+++ zBonus_exercises/Variables.py
@@ -0,0 +1,8 @@
+'''Assign a message to a variable, and print that message. Then change the value of the
+variable to a new message, and print the new message.'''
+
+my_msg = "Tomorrow wear red!" #variable
+print (my_msg) #print variable
+
+my_msg = "tomorrow wear green!" #change value in variable
+print (my_msg) #print variable again
