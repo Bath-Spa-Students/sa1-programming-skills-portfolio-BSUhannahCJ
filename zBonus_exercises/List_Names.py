5cc4e50a2f05b9c02e25db8f1576adac353c2610
BSUhannahCJ <hannahchanjudeh@gmail.com>
2024-11-20 22:31:40 +0400

    List_Names
    Bonus exercise 4

---
+++ zBonus_exercises/List_Names.py
@@ -0,0 +1,8 @@
+'''Store the names of a few of your friends in a list called names. Print each person’s name
+by accessing each element in the list, one at a time.'''
+
+names = ['Charlie', 'Shahd', 'Humna'] #list with friends names
+
+print(names[0]) #print first value in list
+print(names[1]) #print second value in list
+print(names[2]) #print third value in list
