23d3726c0a26e3d4c311276367d51bf4c7b21f7e
BSUhannahCJ <hannahchanjudeh@gmail.com>
2024-11-20 22:43:15 +0400

    Control Structures
    Bonus exercise  5

---
+++ zBonus_exercises/Control Structures.py
@@ -0,0 +1,25 @@
+'''Imagine an alien was just shot down in a game. Create a variable called alien_color and
+assign it a value of 'green', 'yellow', or 'red'.
+•Write an if statement to test whether the alien’s color is green. If it is, print a message
+that the player just earned 5 points.
+•Write one version of this program that passes the if test and another that fails. (The
+version that fails will have no output.)'''
+
+#the one that works
+alien_color = "yellow" #variable with alien color
+
+if alien_color == "yellow": #if the variable is equal to yellow
+    print ("Player one earned 5 points!") #it prints this
+
+
+#the one that fails
+
+alien_color = "red" #variable with alien color
+
+if alien_color == "yellow": #if the variable is equal to yellow
+    print ("Player one earned 5 points!") #it prints this
+    #else nothing prints
+
+
+
+
