a1b89813dc85c6d9226ab8e33854899728d004a0
BSUhannahCJ <hannahchanjudeh@gmail.com>
2024-11-20 22:19:08 +0400

    Strings
    Bonus exercise 1

---
+++ zBonus_exercises/Strings.py
@@ -0,0 +1,8 @@
+'''Write three strings in different variables and print the output as one string.'''
+
+Fst_word = "Welcome" #1st string
+Snd_word = "to" #2nd string
+Trd_word = "BSU!" #3rd string
+
+#string concatenation through print 
+print(Fst_word, Snd_word, Trd_word)
