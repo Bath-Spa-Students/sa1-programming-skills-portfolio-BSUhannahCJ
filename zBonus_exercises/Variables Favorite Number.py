154ca4ad44ef187b8059914c1f028f417126a1ce
BSUhannahCJ <hannahchanjudeh@gmail.com>
2024-11-20 22:27:14 +0400

    Variables Favorite Number
    Bonus exercise 3

---
+++ zBonus_exercises/Variables Favorite Number.py
@@ -0,0 +1,7 @@
+'''Use a variable to represent your favorite number. Then, using that variable, create a
+message that reveals your favorite number. Print that message.'''
+
+Fav_Num = 17 #variable with favorite number
+
+#string concatenation through print 
+print ("My favorite number is " + str(Fav_Num) + "!") #converting the int to a str to be able to concatenate
